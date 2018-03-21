bde6618c14286a84684bfc904dc505384554f1bd
alvaro.govone <alvaro.govone@waiodev.com.br>
2018-03-21 14:24:20 -0300

    Primeiro commit (issue #1)

---
+++ src/main/java/br/com/abg/coffeejar/api/resource/ClientResource.java
@@ -0,0 +1,17 @@
+package br.com.abg.coffeejar.api.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ *
+ * @author <a href="mail:alvaro.govone@waiodev.com.br">Alvaro Govone</a>
+ *
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("/clients")
+public class ClientResource {
+
+}
